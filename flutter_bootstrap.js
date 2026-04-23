11a8f41858d43139dfa313235f9bd82e593c62fa
srilankasair-cmyk <srilankasair-cmyk@users.noreply.github.com>
2026-04-23 15:56:01 +0000

    deploy: 7ec45cb7272739c8c85092695cba32d8369b13fd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4135124306" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "720446679" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
